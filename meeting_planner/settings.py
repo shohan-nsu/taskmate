db2e40e38997b2f65a56fbb703241f52c2ef4efa
Shohan Khan <shohan806@outlook.com>
2022-11-10 23:44:36 +0600

    project

--- meeting_planner/settings.py
+++ meeting_planner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django-heroku
 import os
 import environ
 
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
