99c65ea4e0e444fb6e124a56f4b61dca632b4dbb
Shohan Khan <shohan806@outlook.com>
2022-11-11 00:11:39 +0600

    added

--- meeting_planner/settings.py
+++ meeting_planner/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django-heroku
+import heroku as heroku
+import django_heroku
 import os
 import environ
 
